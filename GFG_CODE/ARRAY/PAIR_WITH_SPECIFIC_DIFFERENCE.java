98fa6edac83b81f764593dab3d7fb0cdebfb6149
ANKIT CHAUDHARY <42476350+ANKITCHAUDHARY-15@users.noreply.github.com>
2019-01-29 00:35:03 +0630

    Update PAIR_WITH_SPECIFIC_DIFFERENCE.java
    https://practice.geeksforgeeks.org/problems/pairs-with-specific-difference/0
    https://www.geeksforgeeks.org/maximum-sum-pairs-specific-difference/

--- GFG_CODE/ARRAY/PAIR_WITH_SPECIFIC_DIFFERENCE.java
+++ GFG_CODE/ARRAY/PAIR_WITH_SPECIFIC_DIFFERENCE.java
@@ -42,3 +42,46 @@ class GFG {
 	    return max_i;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//.............second method..........time-o(n) space-o(1)...................
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=sc.nextInt();
+		    }
+		    int k=sc.nextInt();
+		    Arrays.sort(arr);
+		    int sum=0;
+		    for(int i=n-1;i>0;i--){
+		        if(arr[i]-arr[i-1]<k){
+		            sum+=arr[i]+arr[i-1];
+		            i--;
+		        }
+		    }
+		    System.out.println(sum);
+	    }
+	}
+}
