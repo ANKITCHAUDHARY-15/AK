2d8697a6f3530f6dab7a20d6ce487594b86344fd
ANKIT CHAUDHARY <42476350+ANKITCHAUDHARY-15@users.noreply.github.com>
2019-02-03 00:47:26 +0630

    Create **Finding Position.java
    https://practice.geeksforgeeks.org/problems/finding-position/0

---
+++ GFG_CODE/MATH/**Finding Position.java
@@ -0,0 +1,16 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    System.out.println((int)Math.pow(2,(int)(Math.log(n)/Math.log(2))));
+		}
+	}
+}
